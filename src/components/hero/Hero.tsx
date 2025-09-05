cae431058cac7a93b1e0007d96c64d5bc671fd10
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2025-09-05 17:27:58 +0300

    fix: move MagicButton link inside the content container for better layout

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -3,15 +3,23 @@ import { Spotlight } from '../ui/Spotlight'
 import { cn } from '@/lib/utils'
 import { TextGenerateEffect } from '../ui/TextGenerateEffect'
 import MagicButton from '../ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa6'
 
 const Hero = () => {
     return (
         <div className='pb-20 pt-36 '>
-            <div >
-                <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
-                <Spotlight className="top-10 -left-full h-[80vh] w-[50vw]" fill="purple" />
-                <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+            <div>
+                <Spotlight
+                    className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+                    fill="white"
+                />
+                <Spotlight
+                    className="h-[80vh] w-[50vw] top-10 left-full"
+                    fill="purple"
+                />
+                <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
             </div>
+
 
             <div className=" absolute top-0 left-0  flex h-screen w-full items-center justify-center bg-white dark:bg-black-100 dark:bg-grid-white bg-grid-black">
                 <div
@@ -30,9 +38,9 @@ const Hero = () => {
                     <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Dynamic Web Magic with Next.js</h2>
                     <TextGenerateEffect className='text-center text-[40px] md:text-50xl lg:text-6xl' words='Transforming Concepts into Seamless User Experiences' />
                     <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">Hi, I&apos;m Mykola, a Next.js Developer based in Ukraine.</p>
+                    <a href="#about"><MagicButton title="Show my work" icon={<FaLocationArrow />} position='right' /></a>
                 </div>
             </div>
-            <a href="#about"><MagicButton title="Show my work" /></a>
 
 
 
--- src/components/ui/MagicButton.tsx
+++ src/components/ui/MagicButton.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 type Props = {
     title: string;
     icon: React.ReactNode;
-    position: string;
+    position: 'left' | 'right';
     handleClick?: () => void;
     otherClasses?: string;
 }
@@ -12,8 +12,10 @@ const MagicButton = ({ title, icon, position, handleClick, otherClasses }: Props
     return (
         <button className="relative inline-flex h-12  w-full overflow-hidden rounded-lg p-[1px] focus:outline-none md:w-60 md:mt-10">
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-            <span className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl ${otherClasses}`} onClick={handleClick}>
+            <span className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`} onClick={handleClick}>
+                {position === 'left' && icon}
                 {title}
+                {position === 'right' && icon}
             </span>
         </button>)
 }
