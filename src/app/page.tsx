9da3362ba863ebb15c2a1181d18c01500ffc8c2e
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2025-09-10 10:19:00 +0300

    feat: enhance layout and navigation components with improved styling and functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,16 @@ import Grid from "@/components/grid/Grid";
 import Hero from "@/components/hero/Hero";
 import RecentProjects from "@/components/recent-porjects/recent-porjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome } from "react-icons/fa";
+import { navItems } from "@/data";
+
+
 
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[
-          { name: "Home", link: "/", icon: <FaHome /> },
-          { name: "About", link: "#about" },
-          { name: "Projects", link: "#projects" },
-          { name: "Contact", link: "#contact" },
-        ]} />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
