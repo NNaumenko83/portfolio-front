4fb824b4cc59d46a19c3999e059b139fbd1a9fc1
Mykola <nnv2006@ukr.net>
2025-09-12 11:32:33 +0300

    feat: add Footer component with contact information and social media links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Approach from "@/components/approach/approach";
 import Experience from "@/components/experience/experience";
+import Footer from "@/components/footer/footer";
 import Grid from "@/components/grid/Grid";
 import Hero from "@/components/hero/Hero";
 import RecentProjects from "@/components/recent-porjects/recent-porjects";
@@ -12,7 +13,7 @@ import { navItems } from "@/data";
 export default function Home() {
 
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
@@ -20,6 +21,7 @@ export default function Home() {
         <RecentProjects />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
