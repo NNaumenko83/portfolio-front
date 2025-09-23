7d9d54f3162175a9b0fcbeea5467207473954633
Mykola <nnv2006@ukr.net>
2025-09-23 10:39:29 +0300

    fix: update import path for motion and adjust PinPerspective width for consistency

--- src/components/ui/Pin.tsx
+++ src/components/ui/Pin.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React, { useState } from "react";
-import { motion } from "framer-motion";
-
+import { motion } from "motion/react";
 import { cn } from "@/lib/utils";
+
 
 export const PinContainer = ({
     children,
@@ -36,6 +36,7 @@ export const PinContainer = ({
             )}
             onMouseEnter={onMouseEnter}
             onMouseLeave={onMouseLeave}
+
         >
             <div
                 style={{
@@ -48,7 +49,6 @@ export const PinContainer = ({
                     style={{
                         transform: transform,
                     }}
-                    // remove  bg-black
                     className="absolute left-1/2 p-4 top-1/2  flex justify-start items-start  rounded-2xl  shadow-[0_8px_16px_rgb(0_0_0/0.4)] border border-white/[0.1] group-hover/pin:border-white/[0.2] transition duration-700 overflow-hidden"
                 >
                     <div className={cn(" relative z-50 ", className)}>{children}</div>
@@ -67,8 +67,7 @@ export const PinPerspective = ({
     href?: string;
 }) => {
     return (
-        // change w-96 to w-full
-        <motion.div className="pointer-events-none w-full h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
+        <motion.div className="pointer-events-none  w-96 h-80 flex items-center justify-center opacity-0 group-hover/pin:opacity-100 z-[60] transition duration-500">
             <div className=" w-full h-full -mt-7 flex-none  inset-0">
                 <div className="absolute top-0 inset-x-0  flex justify-center">
                     <a
