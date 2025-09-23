fb6c0c377880b580fcb013f4f87fc40b977da096
Mykola <nnv2006@ukr.net>
2025-09-23 12:31:59 +0300

    fix: adjust layout and styling in RecentProjects component for improved visual consistency

--- src/components/recent-porjects/recent-porjects.tsx
+++ src/components/recent-porjects/recent-porjects.tsx
@@ -16,25 +16,27 @@ const RecentProjects = () => {
             <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
                 {projects.map((item) => (
                     <div
-                        className="lg:min-h-[34rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+                        className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
                         key={item.id}
                     >
                         <PinContainer
                             title="/ui.aceternity.com"
                             href="https://twitter.com/mannupaaji"
                         >
-                            <div className="rounded-3xl relative flex items-center justify-center sm:w-96 w-[80vw] h-[20vh] lg:h-[30vh] mb-10 overflow-hidden">
+                            <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[200px] lg:h-[300px] mb-10 ">
                                 <div
-                                    className="relative w-full h-full overflow-hidden  "
+                                    className="relative w-full h-full overflow-hidden lg:rounded-3xl"
                                     style={{ backgroundColor: "#13162D" }}
                                 >
-                                    <img src="/bg.png" alt="bgimg" className="" />
+                                    <img src="/bg.png" alt="bgimg" />
+                                    <img
+                                        src={item.img}
+                                        alt="cover"
+                                        className="z-10 absolute bottom-0 rotate-6
+                                        "
+                                    />
                                 </div>
-                                <img
-                                    src={item.img}
-                                    alt="cover"
-                                    className="z-10 absolute bottom-0"
-                                />
+
                             </div>
 
                             <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
