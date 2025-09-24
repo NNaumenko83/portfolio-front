df223f18ed4db34b3eb86a0a9bb578b536dc1ad5
Mykola <nnv2006@ukr.net>
2025-09-24 16:30:07 +0300

    fix: update link in Hero component to point to projects section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -40,7 +40,7 @@ const Hero = () => {
                     <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
                         Hi, I&apos;m Mykola — Frontend Developer (React, Next.js) based in Ukraine.
                     </p>
-                    <a href="#about"><MagicButton title="Show my work" icon={<FaLocationArrow />} position='right' /></a>
+                    <a href="#projects"><MagicButton title="Show my work" icon={<FaLocationArrow />} position='right' /></a>
                 </div>
             </div>
 
