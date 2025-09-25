dfdef24ce40ff5f6080c9523d4caa89e66443ed2
Mykola <nnv2006@ukr.net>
2025-09-25 10:18:33 +0300

    fix: update social media links in footer to ensure proper navigation

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
             <div className='flex mt-16 md:flex-row flex-col justify-between items-center '>
                 <p className="md:text-base text-sm md:font-normal font-light ">Copyright © 2025 Mykola Naumenko</p>
                 <div className="flex items-center md:gap-3 gap-6">
-                    {socialMedia.map((profile) => <div className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300" key={profile.id}> <img src={profile.img} alt={profile.img} className="w-5 h-5" width={20} height={20} /> </div>)}
+                    {socialMedia.map((profile) => <a href={profile.link} target='_blank' rel="noopener noreferrer" className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300" key={profile.id}> <img src={profile.img} alt={profile.img} className="w-5 h-5" width={20} height={20} /> </a>)}
                 </div>
             </div>
 
